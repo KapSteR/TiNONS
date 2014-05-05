b4ead699c0fb43e2e0d7183899ca7358c2bc37da
KapSteR <hr.kasper.nielsen@gmail.com>
2014-05-05 13:32:05 +0200

    Fix linear discr. with 2 speakers

--- Matlab/Process_Data.m
+++ Matlab/Process_Data.m
@@ -62,30 +62,40 @@ featureSpace = [
 
 %% Linear things
 
-x = featureSpace';
-x1 = featuresJacob';
+x = featureSpace;
+x1 = featuresJacob;
 x2 = featuresCamilla';
 
-m_1 = mean(x1,2); M_1 = diag(m_1)*ones(size(x1));
-m_2 = mean(x2,2); M_2 = diag(m_2)*ones(size(x2));
+N1 = length(x1);
+N2 = length(x2);
 
-S_W = (x1-M_1)*(x1-M_1)'+(x2-M_2)*(x2-M_2)'
+t(:,1) = [ones(N1,1) ; zeros(N2,1)];
+t(:,2) = [zeros(N1,1) ; ones(N2,1)];
 
-w = inv(S_W)*(m_1-m_2);
+% m_1 = mean(x1,2); M_1 = diag(m_1)*ones(size(x1));
+% m_2 = mean(x2,2); M_2 = diag(m_2)*ones(size(x2));
 
+
+
+% S_W = (x1-M_1)*(x1-M_1)'+(x2-M_2)*(x2-M_2)';
+
+% w = inv(S_W)*(m_1-m_2);
 
 x0 = 1;
-x_ = [ones(size(x,1)) x];
+x_ = [x ones(size(x,1),1)];
+
+w = inv(x_'*x_)*x_'*t;
 
 % w_ = [w0 w9];
 
-y = w'*x_;
+y = w'*x_';
 
 
 r = y / norm(w);
 
-plot(r)
+class = (y(1,:) > y(2,:))'
 
+plot(class)
 
 
 
@@ -105,29 +115,29 @@ plot(r)
 
 
 
-%% Dimentionality reduction
-
-
-
-%% Probabilistic methods
-%% Gaussian Mixture Models
-
-nClasses = 2; %numel(names);
-
-GMM = fitgmdist(featureSpace,nClasses)
-
-idx = cluster(GMM,featureSpace);
-
-cluster1 = (idx == 1);
-cluster2 = (idx == 2);
-cluster3 = (idx == 3);
-cluster4 = (idx == 4);
-clusterML = [cluster1 cluster2 cluster3 cluster4];
-
-
-%% Present result
-
-plot(idx,'.')
+% %% Dimentionality reduction
+% 
+% 
+% 
+% %% Probabilistic methods
+% %% Gaussian Mixture Models
+% 
+% nClasses = 2; %numel(names);
+% 
+% GMM = fitgmdist(featureSpace,nClasses)
+% 
+% idx = cluster(GMM,featureSpace);
+% 
+% cluster1 = (idx == 1);
+% cluster2 = (idx == 2);
+% cluster3 = (idx == 3);
+% cluster4 = (idx == 4);
+% clusterML = [cluster1 cluster2 cluster3 cluster4];
+% 
+% 
+% %% Present result
+% 
+% plot(idx,'.')
 
 
 
