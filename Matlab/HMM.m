3cae399b50744379bc1922aa5d7d3250471deaa6
KapSteR <hr.kasper.nielsen@gmail.com>
2014-05-22 09:54:26 +0200

    Start HMM

--- Matlab/HMM.m
+++ Matlab/HMM.m
@@ -1,4 +1,33 @@
 %% Hidden Markov Model
 clear; clc;
 
+addpath(genpath('HMMall'))
 
+disp('Input Data')
+
+tic
+
+load('DATA\TrainingSetPCA.mat');
+x_train = x_train(:,1:end-1); % Remove bias column of 1's 
+
+
+%% Setup HMM
+
+N_hiddenstates = 60;        % number of hidden states
+Ndim = size(x_train,2);     % dimensions (of feature vector)
+Niter = 100;                % max iteration of EM-alg
+
+% Train hmm on class 1
+% Train model on training data
+% initial guess of parameters - randomly
+N = 592;
+Markov = struct('LL', {{}}, 'prior', {{}}, 'transmat', {{}}, 'mu', {{}}, 'sigma', {{}}, 'mixmat',{{}});
+
+
+
+
+
+
+%% END
+disp('End')
+toc
