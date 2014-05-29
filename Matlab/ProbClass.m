73927a895195ba37e0a3889232af3b72a105110a
KapSteR <hr.kasper.nielsen@gmail.com>
2014-05-29 14:14:11 +0200

    Finish PGM in matlab

--- Matlab/ProbClass.m
+++ Matlab/ProbClass.m
@@ -21,20 +21,148 @@ x_test = x_test(:,1:end-1);% Remove bias column of 1's
 [value, t_test] = max(t_test');
 % t_test = t_test(1:Nt1+Nt2)';
 
+nSpeakers = numel(names);
+nFeatures = size(x_train,2);
+toc
+
 %% Training
 disp('Training')
 
-mu1 = mean(x_train(1:N1,:));
-S1 = cov(x_train(1:N1,:));
-PD1 = fitdist(x_train(1:N1,:),'Normal');
+mu = zeros(nFeatures,nSpeakers);
+S  = zeros(nFeatures,nFeatures,nSpeakers);
+PD = cell(nSpeakers,1);
+
+for k = 1:nSpeakers
+    
+    mu(:,k)  = mean(x_train(t_train == k,:));
+    S(:,:,k) = cov(x_train(t_train == k,:));
+    toc
+    
+end
+
+
+%% Classify
+disp('Classify')
+
+probVec = zeros(size(t_test,2),nSpeakers);
+posterior = zeros(size(t_test,2),nSpeakers);
+
+for k = 1:nSpeakers
+    
+    probVec(:,k) = mvnpdf(x_test,mu(:,k)',S(:,:,k));
+    
+    
+    numer = mvnpdf(x_test,mu(:,k)',S(:,:,k))*1/nSpeakers;
+    
+    denom = zeros(size(t_test))';
+    for j = 1:nSpeakers
+        if j ~= k 
+            denom = denom + mvnpdf(x_test,mu(:,j)',S(:,:,j))*1/nSpeakers;
+        end
+    end
+    
+    posterior(:,k) = numer ./ denom;
+
+end
 toc
 
-mu2 = mean(x_train(N1+1:N1+N2,:));
-S1 = cov(x_train(N1+1:N1+N2,:));
-PD2 = fitdist(x_train(N1+1:N1+N2,:),'Normal');
+
+
+
+
+
+[value, class] = max(posterior');
+class = class-1;
+
+classTarget = t_test-1;
 toc
 
-mu3 = mean(x_train(N1+N2+1:N1+N2+N3,:));
-S3 = cov(x_train(N1+N2+1:N1+N2+N3,:));
-PD3 = fitdist(x_train(N1+N2+1:N1+N2+N3,:),'Normal');
+%% Show results
+disp('Show Results')
+
+h = figure(2);
+subplot(211)
+plot(class,'+b')
+title('Classification')
+xlabel('Frames - 10 ms/frame')
+ylabel('Class estimate')
+% set(gca,'plotboxaspectratio',aspect)
+subplot(212)
+h = plot(classTarget, 'or')
+title('Target')
+xlabel('Frames - 10 ms/frame')
+ylabel('Class target')
+% set(gca,'plotboxaspectratio',aspect)
+
+
+%% Make LaTeX
+set(gcf, 'PaperPositionMode', 'manual');
+set(gcf, 'PaperUnits', 'centimeters');
+set(gcf, 'PaperPosition', [2 1 18 10]);
+
+
+
+
+
+% Confusion matrix
+confMatrix = OurConfMat(classTarget,class);
+confMatrix
+
+disp(['Accuracy is: ', num2str(confMatrix(end)*100), '%'])
+names
+
+% Make image
+
+figurePath = '..\Document\Appendix\Figures';
+this = pwd
+cd(figurePath)
+print -f2 -r600 -depsc PGM_1digit
+cd(this)
+
+
+
+disp('')
+conMatLatex.tableCloumnHeaders = {
+    ['Speaker ', char(names(1))]
+    ['Speaker ', char(names(2))]
+    ['Speaker ', char(names(3))]
+    'Precision [\%]'
+    };
+
+conMatLatex.tableRowLabels = {
+    ['Estimate ', char(names(1))]
+    ['Estimate ', char(names(2))]
+    ['Estimate ', char(names(3))]
+    'Sensitivity [\%]'
+    };
+
+conMatLatex.tableData = confMatrix;
+conMatLatex.tableData(end,:) = conMatLatex.tableData(end,:)*100;
+conMatLatex.tableData(1:end-1,end) = conMatLatex.tableData(1:end-1,end)*100;
+
+conMatLatex.tableDataRowFormat = {'%.1f'};
+
+% Column alignment ('l'=left-justified, 'c'=centered,'r'=right-justified):
+conMatLatex.tableColumnAlignment = 'c';
+
+% Switch table borders on/off:
+conMatLatex.tableBorders = 1;
+
+% LaTex table caption:
+conMatLatex.tableCaption = 'Confusion matrix - 1 digit';
+
+% LaTex table label:
+conMatLatex.tableLabel = 'PGM_conf_1';
+
+% Switch to generate a complete LaTex document or just a table:
+conMatLatex.makeCompleteLatexDocument = 0;
+
+% Now call the function to generate LaTex code:
+latex = latexTable(conMatLatex);
+
 toc
+
+
+
+
+
